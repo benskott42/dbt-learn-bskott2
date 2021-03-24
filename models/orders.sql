37f073b0f052f97df950ee007681125dda0ddf1e
bskott@gmail.com <bskott@gmail.com>
2021-03-24 19:44:56 +0000

    more tests

--- models/orders.sql
+++ models/orders.sql
@@ -1,7 +1,8 @@
-select p.order_id,
+select p.order_id as order_id,
     o.customer_id,
-    p.payment_total,
-    order_date
+    sum(p.payment_total) as payment_total,
+    min(order_date) as order_date
     from {{ ref('stg_payments') }} p
 left join {{ ref('stg_orders') }}  o on p.order_id = o.order_id
 left join {{ ref('stg_customers') }} c on o.customer_id = c.customer_id
+group by 1,2
--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -2,5 +2,5 @@ select
   id as payment_id,
   orderid as order_id,
   amount / 100 as payment_total
-   from raw.stripe.payment
+   from {{ source('stripe', 'payment') }}
    where status = 'success'

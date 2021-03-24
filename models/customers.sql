b38246f05048254e7db1b577fa5bdf0cf6a15b91
bskott@gmail.com <bskott@gmail.com>
2021-03-24 20:06:20 +0000

    refactored

--- models/customers.sql
+++ models/customers.sql
@@ -16,22 +16,12 @@ customer_orders as (
         customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
-    from orders
-
-    group by 1
-
-),
-
-customer_revenue as (
-
-    select
-        customer_id,
+        count(order_id) as number_of_orders,
         sum(payment_total) as lifetime_value
--- summing up payments from different orders by customer
+        -- summing up payments from different orders by customer
     from orders
     group by 1
+
 ),
 
 final as (
@@ -43,12 +33,12 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        customer_revenue.lifetime_value
+        customer_orders.lifetime_value
 
     from customers
 
     left join customer_orders using (customer_id)
-    left join customer_revenue using (customer_id)
+
 
 
 )
